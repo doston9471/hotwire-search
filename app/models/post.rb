ae7c98c88451a178353b93286d24e302776cf84a
doston9471 <doston.7194@gmail.com>
2023-08-15 15:26:14 +0200

    add rspec && add crud > request/routing tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+
   def self.ransackable_attributes(auth_object = nil)
     ["title", "description", "body"]
   end
+
   def self.ransackable_associations(auth_object = nil)
     []
   end
